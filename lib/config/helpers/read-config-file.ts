53c49d92fa76ccdb9b046993981d1025053d8a7e
Guilherme Tod <guilhermetod@gmail.com>
2021-05-03 15:22:59 -0300

    Fix: correctly handle configPath not found

--- lib/config/helpers/read-config-file.ts
+++ lib/config/helpers/read-config-file.ts
@@ -1,22 +1,22 @@
 import { cosmiconfig } from 'cosmiconfig';
+import { CosmiconfigResult } from 'cosmiconfig/dist/types';
 
 import { LintrConfig } from '@lib/typings/lintr-config';
 import { exitWithError } from '@lib/utils/exit-with-error';
 
 export const readConfigFile = async (configPath?: string | null): Promise<LintrConfig | never> => {
   const configExplorer = cosmiconfig('lintr');
+  const message = 'could not find configuration file';
 
-  const configFile = await (
+  const { config } = await (
     configPath
-      ? configExplorer.load(configPath)
-      : configExplorer.search()
-  );
-
-  const { config } = configFile ?? {};
-
-  if (!config) {
-    exitWithError('could not find configuration file');
-  }
+      ? configExplorer
+        .load(configPath)
+        .catch(() => exitWithError(`${message} (${configPath})`))
+      : configExplorer
+        .search()
+        .then((configFile) => configFile ?? exitWithError(message))
+  ) as NonNullable<CosmiconfigResult>;
 
   return config;
 };
