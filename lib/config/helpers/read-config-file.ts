dec59ce2b88178bf04d6006eeb0986b075256815
Guilherme Tod <guilhermetod@gmail.com>
2021-05-03 13:05:24 -0300

    Refactor: add never as a possible return on readConfigFile

--- lib/config/helpers/read-config-file.ts
+++ lib/config/helpers/read-config-file.ts
@@ -3,7 +3,7 @@ import { cosmiconfig } from 'cosmiconfig';
 import { LintrConfig } from '@lib/typings/lintr-config';
 import { exitWithError } from '@lib/utils/exit-with-error';
 
-export const readConfigFile = async (configPath?: string | null): Promise<LintrConfig> => {
+export const readConfigFile = async (configPath?: string | null): Promise<LintrConfig | never> => {
   const configExplorer = cosmiconfig('lintr');
 
   const configFile = await (
