3ac5152570bcd7e9f1db0f719af98e1570e378ee
Guilherme Tod <guilhermetod@gmail.com>
2022-02-23 21:37:20 -0300

    Fix: remove transloco keys manager fix flag

--- lib/presets/transloco-keys.ts
+++ lib/presets/transloco-keys.ts
@@ -5,7 +5,6 @@ export const translocoKeys = (config: Partial<Linter> = {}): Linter => ({
   name: 'Transloco keys',
   icon: icons.translation,
   command: 'transloco-keys-manager find',
-  fixFlag: '-a',
   forceRoot: true,
   ...config,
 }); 
